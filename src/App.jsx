5e3c7e6217e0b1cbb01a4d449c94393b5c79b7f9
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2024-10-11 14:36:50 +0530

    3d-nike-card

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './components/Card'
+const App = () => {
+  return <div className='w-full bg-[#b1a29d] h-screen flex items-center justify-center '>
+
+    <Card></Card>
+  </div>
+};
+
+export default App;
